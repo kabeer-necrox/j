5f349e3a940045a7f17e29c5aba6efde0441923c
kabeer-necrox <khushkabeer@gmail.com>
2023-10-08 10:13:31 +0300

    codee

--- app.js
+++ app.js
@@ -50,13 +50,64 @@
 
 
 
-var arr = ["ali", 12, true, 100]
-// console.log(arr[2])
+// var arr = ["ali", ,12, true, 100]
+// // console.log(arr[2])
 
-let [firstValue,secondValue,thirdValue]=arr
+// let [firstValue,secondValue='kabeer',thirdValue]=arr
 
 
-console.log(firstValue,secondValue,thirdValue)
+// // console.log(firstValue,secondValue,thirdValue)
+// console.log(firstValue)
+// console.log(secondValue)
+// console.log(thirdValue)
+
+
+// *****************de-structring of obj**************************
+
+// let obj ={
+//     name: "kabeer",
+//     age: 21,
+//     city:"nyc",
+//     address: "xyz"
+// }
+// // console.log(obj.age)
+// let{name,age,city,address}= obj
+// console.log(name,age,city)
+
+
+
+// ****************ternory operator*****************
+
+
+
+// let age = 10;
+// if(age < 20){
+//     console.log("allowed")
+// }else{
+//     console.log("not allowed")
+// }
+ 
+var age = 20;
+
+let check = age < 20 ? console.log("allowed"):console.log("not allowd")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
