c6ac264093c955aebefe73a33a0bf48f36ff4a1b
kabeer-necrox <khushkabeer@gmail.com>
2023-10-08 09:50:17 +0300

    code

--- app.js
+++ app.js
@@ -17,5 +17,62 @@
 
 
 // ******let and const in js********
+// if(true){
+//     let b = "yses"
+//     // let is working as local variable
+//     var a =   "hello world"
+// }
+// console.log(a)
+
+
+
+
+// let fName = "abc"
+// fName = "xyz"
+// console.log(fName)
+
+
+// *******************js template leateral*******************
+
+
+// let fName= "abc"
+// let lName = "xyz"
+// // console.log(fName + " " + lName)
+
+
+// console.log(`welcome to ${fName} ${lName}`)
+
+// ***************de structrig of arry********************************
+
+
+
+
+
+
+
+var arr = ["ali", 12, true, 100]
+// console.log(arr[2])
+
+let [firstValue,secondValue,thirdValue]=arr
+
+
+console.log(firstValue,secondValue,thirdValue)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
