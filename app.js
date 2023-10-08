3cde90f55c29151a73332107747a97dd2c0abf58
kabeer-necrox <khushkabeer@gmail.com>
2023-10-08 09:18:52 +0300

    advance js

---
+++ app.js
@@ -0,0 +1,21 @@
+// ************ Advance javascript is here***************
+
+
+
+// var a = "hello world"
+
+
+// function greet(){
+//     a = "heloo advance javascript"
+//     console.log(a)
+// }
+// greet()
+
+// console.log(a)
+
+
+
+
+// ******let and const in js********
+
+
