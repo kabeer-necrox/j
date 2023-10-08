4484406302e92c7904f3a4b31922f5cecdfe1af4
kabeer-necrox <khushkabeer@gmail.com>
2023-10-08 23:13:13 +0300

    reason

--- app.js
+++ app.js
@@ -97,12 +97,223 @@
 // ******************spread operator**************
 
 
-let arr = [1,2,3,4,5,6,7,8,9]
-let arr1 = [...arr,"ali", "kbeer","nex"]
+// let arr = [1,2,3,4,5,6,7,8,9]
+// let arr1 = [...arr,"ali", "kbeer","nex"]
 
 
-// let result = arr.concat(arr1)
-console.log(arr1)
+// // let result = arr.concat(arr1)
+// console.log(arr1)
+
+
+
+// ******************// include also string method*******************
+
+
+// let fName =  "Muhammad";
+// console.log(fName.includes('Mu'))
+
+// console.log(fName.substring(1,2))
+
+
+
+
+// math method in es6
+
+
+// let number = 5.87;
+// console.log(Math.sign(number))
+
+
+
+// let number = 5;
+// console.log(Math.sqrt(number))
+
+
+
+// Asy && sync in js
+//  console.log(" number one")
+//  console.log("numner two ")
+//  console.log("number three")
+
+// console.log("1")
+// setTimeout(() => {
+//     console.log("first nuber after three second")
+    
+// }, 3000);
+// console.log('no second is taking is')
+
+
+
+// *****************fat arrow function in es6*****************
+
+
+// function greet(){
+//     console.log("hello greeting")
+// }
+// greet()
+
+
+
+
+
+
+// var greet = function()
+// {
+//     alert("hello")
+// }
+
+// greet()
+
+
+
+// let greet = () =>{
+//     alert("hellow world");
+//     // console.log("hello fat arrow functio")
+// }
+// greet()
+
+
+
+// let greet = (val1, num1) =>{
+//     console.log(cal1, num1)
+//     return num1 + val1
+// }
+
+// greet(20,12)
+
+// let greet = (num1,num2) => num1 + num2;
+// console.log(greet(20,10))
+
+// let greet = (num1,num2) =>  num1 + num2
+// console.log(greet(30,1))
+
+
+
+// ************pass by value and pass by reference****************
+
+let a = 10;
+let b = a;
+b = b + 20;
+console.log(b)
+console.log(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
